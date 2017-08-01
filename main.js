0691e7e63f810df48abe6440ae0cfd5311497372
ben harrington <harringtonben@icloud.com>
2017-07-31 19:03:47 -0500

    added cat to html

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ var cat = "fluffy";
 console.log("hello world", cat);
 console.log("fixed the emergency");
 
+var catzContainer = document.getElementById("catz");
+
+catzContainer.innerHTML = '<h1>' + cat + '</h1>';
+
+
+console.log(catzContainer);
